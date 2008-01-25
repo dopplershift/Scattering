3be7444e8d3994f72172a47b231dc10ff6938867
dopplershift <rmay31@gmail.com>
2008-01-25 19:54:53 +0000

    Clean up quicktest to run under new layout.

--- test/quicktest.py
+++ test/quicktest.py
@@ -1,6 +1,8 @@
-import numpy as N
-import tmatrix as tmat
+from scattering import _tmatrix as tmat
 from scattering import refractive_index
 m = refractive_index('water',3.0)
 ds = 5.0
 qs,fmat,bmat= tmat.tmatrix(ds/2.0,1.0,30.0,m,1.0,-1)
+print qs
+print fmat
+print bmat
