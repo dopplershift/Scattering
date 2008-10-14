408d72c16c860386caf5a321f51c77d8988eb468
dopplershift <rmay31@gmail.com>
2008-10-14 17:58:10 +0000

    Clean up compare_scatter test.

--- test/compare_scatter.py
+++ test/compare_scatter.py
@@ -1,26 +1,27 @@
-import matplotlib.pyplot as P
-import numpy as N
+import matplotlib.pyplot as plt
+import numpy as np
 import scattering, dsd
-from constants import cm_per_m, mm_per_m
+import scipy.constants as consts
 
 def plot_csec(scatterer, csec, name):
-  P.semilogy(scatterer.diameters * mm_per_m, csec * cm_per_m**2,
-    label = '%.1f cm (%s)' % (scatterer.wavelength * cm_per_m, scatterer.model))
-  P.xlabel('Diameter (mm)')
-  P.ylabel(r'$%s (cm^2)$' % name)
+    plt.semilogy(scatterer.diameters / consts.milli, csec / consts.centi**2,
+        label = '%.1f cm (%s)' % (scatterer.wavelength / consts.centi,
+        scatterer.model))
+    plt.xlabel('Diameter (mm)')
+    plt.ylabel(r'$\rm{%s (cm^2)}$' % name)
 
 def plot_csecs(scatterers):
-  for s in scatterers:
-    P.subplot(2,2,1)
-    plot_csec(s, s.sigma_s, '\sigma_{s}')
-    P.subplot(2,2,2)
-    plot_csec(s, s.sigma_a, '\sigma_{a}')
-    P.subplot(2,2,3)
-    plot_csec(s, s.sigma_e, '\sigma_{e}')
-    P.subplot(2,2,4)
-    plot_csec(s, s.sigma_b, '\sigma_{b}')
+    for s in scatterers:
+        plt.subplot(2,2,1)
+        plot_csec(s, s.sigma_s, '\sigma_{s}')
+        plt.subplot(2,2,2)
+        plot_csec(s, s.sigma_a, '\sigma_{a}')
+        plt.subplot(2,2,3)
+        plot_csec(s, s.sigma_e, '\sigma_{e}')
+        plt.subplot(2,2,4)
+        plot_csec(s, s.sigma_b, '\sigma_{b}')
   
-d = N.linspace(0.01, 2.0, 200).reshape(200,1) / cm_per_m
+d = np.linspace(0.01, 2.0, 200).reshape(200,1) * consts.centi
 sband = .1 
 xband = .0321
 
@@ -37,7 +38,5 @@ s_ray = scattering.scatterer(sband, temp, 'water', diameters=d)
 s_ray.set_scattering_model('rayleigh')
 
 plot_csecs([x_mie, x_ray, s_mie, s_ray])
-P.legend(loc = 'lower right')
-P.show()
-
-
+plt.legend(loc = 'lower right')
+plt.show()
