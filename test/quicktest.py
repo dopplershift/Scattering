9662eb70e27f58c98c60c12db04fad60b8afa72f
dopplershift <rmay31@gmail.com>
2008-11-18 22:26:14 +0000

    Modify quicktest to use all of the the scattering methods.

--- test/quicktest.py
+++ test/quicktest.py
@@ -1,8 +1,32 @@
-from scattering import _tmatrix as tmat
+import numpy as np
+from scattering import tmatrix, mie, rayleigh, rayleigh_gans
 from scattering import refractive_index
-m = refractive_index('water',3.0)
-ds = 5.0
-qs,fmat,bmat= tmat.tmatrix(ds/2.0,1.0,30.0,m,1.0,-1)
+
+lam = .1 #meters
+ds = np.array([.001])#meters
+m = refractive_index('water', lam)
+
+fmat,bmat,qs = tmatrix(m, ds, lam * 10.0, 'sphere')
+print 'T-matrix'
 print qs
 print fmat
 print bmat
+
+fmat,bmat,qs = mie(m, ds, lam * 10.0, 'sphere')
+print '\nMie'
+print qs
+print fmat
+print bmat
+
+fmat,bmat,qs = rayleigh(m, ds, lam * 10.0, 'sphere')
+print '\nRayleigh'
+print qs
+print fmat
+print bmat
+
+fmat,bmat,qs = rayleigh_gans(m, ds, lam * 10.0, 'sphere')
+print '\nRayleigh-Gans'
+print qs
+print fmat
+print bmat
+
