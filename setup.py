144003b69598462a1ec5e8f53bef7da40ed4abd0
dopplershift <rmay31@gmail.com>
2010-07-15 10:04:15 -0500

    Rewrite setup.py to add compile flags to those obtained from lapack options, so that we don't end up with duplicate kwargs passed to the extension. Also, change the compile flags to use -march=native instead of -march=opteron.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from numpy.distutils.misc_util import Configuration
 from numpy.distutils.system_info import get_info
 from os.path import join
 
-flags = ['-W', '-Wall', '-march=opteron', '-O3']
+flags = ['-W', '-Wall', '-march=native', '-O3']
 
 def configuration(parent_package='', top_path=None):
     config = Configuration('scattering', parent_package, top_path,
@@ -15,9 +15,10 @@ def configuration(parent_package='', top_path=None):
         url = 'http://weather.ou.edu/~rmay/research.html')
     
     lapack = get_info('lapack_opt')
+    lapack.setdefault('extra_compile_args', []).extend(flags)
     sources = ['ampld.lp.pyf', 'ampld.lp.f', 'modified_double_precision_drop.f']
     config.add_extension('_tmatrix', [join('src', f) for f in sources],
-        extra_compile_args=flags, **lapack)
+        **lapack)
     return config
 
 setup(**configuration(top_path='').todict())
